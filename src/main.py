103af04d7f554b476526f88ae1a9bf6127ef0a36
oytarhanova <oytarhanova@fintechiq.ru>
2022-10-10 11:13:23 +0700

    fix running

--- src/main.py
+++ src/main.py
@@ -1,22 +1,39 @@
 import asyncio
-from tortoise import run_async
+from aerich import Command
 
 from file_process import FileProcess
+from model_description import TORTOISE_ORM
 from postgres.postgres_connection_manager import PostgresConnection
+from postgres.repository import Repository
 from settings import Settings
 
 settings = Settings()
-
+postgres = PostgresConnection(settings)
+repository = Repository()
 file_process = FileProcess()
-file_process.create_model()
 
-run_async(PostgresConnection(settings).init_orm())
 
-from postgres.repository import Repository
+async def init_db():
+    command = Command(tortoise_config=TORTOISE_ORM, app='models')
+    await command.init()
 
 
 async def insert_records():
     for index, row in file_process.data.iterrows():
         await Repository().insert_records(row.to_dict())
 
-asyncio.run(insert_records())
+
+def run():
+    loop = asyncio.get_event_loop()
+    try:
+        tasks = asyncio.gather(
+            init_db(),
+            postgres.init_orm(),
+        )
+        loop.run_until_complete(tasks)
+        loop.run_until_complete(insert_records())
+    finally:
+        loop.run_until_complete(postgres.close_orm())
+
+
+run()
