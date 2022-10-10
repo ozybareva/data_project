a81281a59d83d7a10cd951566fa1d444874c6348
oytarhanova <oytarhanova@fintechiq.ru>
2022-10-10 11:10:47 +0700

    fix model description

--- src/model_description.py
+++ src/model_description.py
@@ -3,14 +3,14 @@ from src.settings import Settings
 setting = Settings()
 
 TORTOISE_ORM = {
-    "connections": {"local": setting.postgres_dsn},
+    "connections": {"default": setting.postgres_dsn},
     "apps": {
         "models": {
             "models": [
                 "src.data_model",
                 "aerich.models"
             ],
-            "default_connection": "local",
+            "default_connection": "default",
         },
     },
 }
